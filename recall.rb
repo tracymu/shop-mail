71707eaa84194488d739cff344bd826498f049fa
tracymu <tracy.musung@moomumedia.com>
2014-03-01 10:30:31 +1100

    Updated the basic Sinatra app to be for my shopping email app

--- recall.rb
+++ recall.rb
@@ -6,89 +6,69 @@ require 'sinatra/flash'
 enable :sessions
 # Not sure what this line means
 
-SITE_TITLE = "Recall"
-SITE_DESCRIPTION = "'Cause you're too busy to remember"
+SITE_TITLE = "Maybe Later"
+SITE_DESCRIPTION = "Treat Yourself If It Goes On Sale"
 
 get '/' do
-    @notes = Note.all :order => :id .desc
-    @title = "All Notes"
-    if @notes.empty?
-      flash[:error] = "No notes found. Add your first below"
-    end
     erb :home
 end
 
 post '/' do
-  n = Note.new
-  n.content = params[:content]
-  n.created_at = Time.now
-  n.updated_at = Time.now
-  if n.save
-    redirect '/', :notice => 'Note created successfully'
+  p = Product.new
+  p.email = params[:email]
+  p.created_at = Time.now
+  p.updated_at = Time.now
+  if p.save
+    redirect '/', :notice => "We'll email you if your item goes on sale."
   else
-    redirect '/', :error => "Failed to save note"
+    redirect '/', :error => "Something went wrong, please try again."
   end
 end
 
-get '/rss.xml' do
-  @notes = Note.all :order => :id .desc
-  builder :rss
-end
 
-get '/:id' do
-  @note = Note.get params[:id]
-  @title = "Edit note ##{params[:id]}"
-  if @note
-    erb :Edit
-  else
-    redirect '/', :error => "Can't find that note"
-  end
-end
+####### Re-instate these later when/if I have a login area ################
 
-put '/:id' do
-  n = Note.get params[:id]
-  n.content = params[:content]
-  n.complete = params[:complete] ? 1:0
-  n.updated_at = Time.now
-  if n.save
-    redirect '/', :notice => 'Note updated successfully'
-  else
-    redirect '/', :error => "Error updating note"
-  end
-end
+# get '/:id' do
+#   @note = Note.get params[:id]
+#   @title = "Edit note ##{params[:id]}"
+#   if @note
+#     erb :Edit
+#   else
+#     redirect '/', :error => "Can't find that note"
+#   end
+# end
 
-get '/:id/delete' do
-  @note = Note.get params[:id]
-  @title = "Confirm deletion of Note ##{params[:id]}"
-  if @note
-    erb :edit
-  else
-    redirect '/', :error => "Can't find that note"
-  end
-end
+# put '/:id' do
+#   n = Note.get params[:id]
+#   n.content = params[:content]
+#   n.complete = params[:complete] ? 1:0
+#   n.updated_at = Time.now
+#   if n.save
+#     redirect '/', :notice => 'Note updated successfully'
+#   else
+#     redirect '/', :error => "Error updating note"
+#   end
+# end
 
-delete '/:id' do
-  n = Note.get params[:id]
-  if n.destroy
-    redirect '/', :notice => "Note deleted successfully"
-  else
-    redirect '/', :error => "Error Deleting Note"
-  end
-end
+# get '/:id/delete' do
+#   @note = Note.get params[:id]
+#   @title = "Confirm deletion of Note ##{params[:id]}"
+#   if @note
+#     erb :edit
+#   else
+#     redirect '/', :error => "Can't find that note"
+#   end
+# end
 
-get '/:id/complete' do
-  n = Note.get params[:id]
-  unless n
-    redirect '/', :error => "Can't find that note"
-  end
-  n.complete = n.complete ? 0:1
-  n.updated_at = Time.now
-  if n.save
-    redirect '/', :notice => "Note marked as complete"
-  else
-    redirect '/', :error => "Error marking note as complete"
-  end
-end
+# delete '/:id' do
+#   n = Note.get params[:id]
+#   if n.destroy
+#     redirect '/', :notice => "Note deleted successfully"
+#   else
+#     redirect '/', :error => "Error Deleting Note"
+#   end
+# end
+
 
 not_found do
   status 404
@@ -98,11 +78,13 @@ end
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
 
-class Note
+class Product
   include DataMapper::Resource
   property :id, Serial
-  property :content, Text, :required => true
-  property :complete, Boolean, :required => true, :default => false
+  property :email, Text, :required => true
+  property :url, Text, :required => true
+  property :price, Integer
+  property :reduced, Boolean, :default => false
   property :created_at, DateTime
   property :updated_at, DateTime
 end
